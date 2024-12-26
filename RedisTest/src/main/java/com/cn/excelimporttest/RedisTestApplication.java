b37f6b5bc6ce0b7f13166e104f71654ffdb37581
whocarewu <1324940338@qq.com>
2024-12-26 15:50:47 +0800

    RedisUtils

--- RedisTest/src/main/java/com/cn/excelimporttest/RedisTestApplication.java
+++ RedisTest/src/main/java/com/cn/excelimporttest/RedisTestApplication.java
@@ -1,14 +1,30 @@
 package com.cn.excelimporttest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Objects;
+
+@Slf4j
 @SpringBootApplication
 public class RedisTestApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(RedisTestApplication.class, args);
+	public static void main(String[] args) throws UnknownHostException {
+		ConfigurableApplicationContext application = SpringApplication.run(RedisTestApplication.class, args);
+		Environment env = application.getEnvironment();
+		String ip = InetAddress.getLocalHost().getHostAddress();
+		String port = env.getProperty("local.server.port");
+		String path = Objects.toString(env.getProperty("server.servlet.context-path"), "").trim();
+		log.info("\n----------------------------------------------------------\n\t" +
+				"Application Demo is running! Access URLs:\n\t" +
+				"Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+				"External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+				"Swagger文档:\thttp://" + ip + ":" + port + path + "/swagger-ui/index.html#/\n" +
+				"----------------------------------------------------------");
 	}
 
 }
--- RedisTest/src/main/java/com/cn/excelimporttest/controller/TestController.java
+++ RedisTest/src/main/java/com/cn/excelimporttest/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.cn.excelimporttest.controller;
 
+import com.cn.excelimporttest.dto.User;
 import com.cn.excelimporttest.uilts.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping
 @RestController
@@ -15,5 +19,14 @@ public class TestController {
     public void test() {
         System.out.println("测试");
         redisUtil.set("test:test:ha", "测试");
+    }
+    @GetMapping("/testList")
+    public void testList() {
+        System.out.println("测试");
+        List<User> list=new ArrayList<>();
+        list.add(new User(1, "测试"));
+        list.add(new User(2, "测试2"));
+        list.add(new User(3, "测试3"));
+        redisUtil.lSet("test:test:2", list);
     }
 }
