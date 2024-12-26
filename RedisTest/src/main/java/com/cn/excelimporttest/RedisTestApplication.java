22677b5eb712e40d9d89476fa0a37aeef716c653
whocarewu <1324940338@qq.com>
2024-12-26 15:20:52 +0800

    RedisUtils 实现

--- RedisTest/src/main/java/com/cn/excelimporttest/RedisTestApplication.java
+++ RedisTest/src/main/java/com/cn/excelimporttest/RedisTestApplication.java
@@ -1,7 +1,9 @@
 package com.cn.excelimporttest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class RedisTestApplication {
